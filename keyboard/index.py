4fcaf26a59f2ecc1e15b04bfbaacac0fe63fa5b5
Nastirniy <nazar.manastirniy6@gmail.com>
2022-08-19 04:17:06 +0300

    dont work

--- keyboard/index.py
+++ keyboard/index.py
@@ -6,7 +6,7 @@ from aiogram.types import (
     InlineKeyboardButton
 )
 
-keyboard = InlineKeyboardMarkup(row_width=2)
+# keyboard = InlineKeyboardMarkup(row_width=2)
 # keyboard.row(
 #     InlineKeyboardButton(text="Register", callback_data="/register"),
 #     InlineKeyboardButton(text="Goods", callback_data="/goods"),
@@ -23,4 +23,21 @@ language_pick_keyboard.row(
     InlineKeyboardButton(text='UA🇺🇦', callback_data="set_localisation_ua"), 
     InlineKeyboardButton(text='RU🇷🇺', callback_data="set_localisation_ru"), 
     InlineKeyboardButton(text='EN🇺🇸', callback_data="set_localisation_en")
+)
+
+registration_valid_keyboard = InlineKeyboardMarkup(row_width=2)
+registration_valid_keyboard.row(
+    InlineKeyboardButton(text='confirm☑️', callback_data="registration_valid_keyboard_confirm"),
+    InlineKeyboardButton(text='edit📝', callback_data="registration_valid_keyboard_edit")
+)
+
+registration_edit_keyboard = InlineKeyboardMarkup(row_width=4)
+registration_edit_keyboard.row(
+    InlineKeyboardButton(text='Name', callback_data="registration_edit_keyboard_name"),
+    InlineKeyboardButton(text='Surname', callback_data="registration_edit_keyboard_surname"),
+).row(
+    InlineKeyboardButton(text='Phone', callback_data="registration_edit_keyboard_phone"),
+    InlineKeyboardButton(text='Email', callback_data="registration_edit_keyboard_email")
+).add(
+    InlineKeyboardButton(text='all again', callback_data="registration_edit_keyboard_all")
 )
