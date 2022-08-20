5416da8f7a0a62758db4d820da4bcf0e45d35d85
Nastirniy <nazar.manastirniy6@gmail.com>
2022-08-20 03:45:23 +0300

    Add re from mail,phone

--- handlers/index.py
+++ handlers/index.py
@@ -14,7 +14,11 @@ def create_handlers(dp: Dispatcher):
     dp.register_message_handler(register_user_name, state=Registration_Form.name)
     dp.register_message_handler(register_user_surname, state=Registration_Form.surname)
     dp.register_message_handler(register_user_email, state=Registration_Form.email)
+    # Registration confirm/edit
+    dp.register_callback_query_handler(register_ending, Text(equals=["set_langage=UA", "set_langage=RU", "set_langage=ENG"]))
+    dp.register_callback_query_handler(register_info_edit, Text(equals=["set_langage=UA", "set_langage=RU", "set_langage=ENG"]))
     # Catalogue
     dp.register_message_handler(open_catalogue, Text(equals="catalogue"))
     # Language
     dp.register_callback_query_handler(set_language, Text(equals=["set_langage=UA", "set_langage=RU", "set_langage=ENG"]))
+    
--- keyboard/index.py
+++ keyboard/index.py
@@ -20,3 +20,22 @@ language_pick_keyboard.row(
     InlineKeyboardButton(text='RU🇷🇺', callback_data="set_langage=RU"),
     InlineKeyboardButton(text='EN🇺🇸', callback_data="set_langage=ENG")
 )
+
+# Keyboard with registration info apply/decline
+registration_valid_keyboard = InlineKeyboardMarkup(row_width=2)
+registration_valid_keyboard.row(
+    InlineKeyboardButton(text='confirm☑️', callback_data="registration_valid_keyboard=confirm"),
+    InlineKeyboardButton(text='edit📝', callback_data="registration_valid_keyboard=edit")
+)
+
+# Keyboard with edit registration info
+registration_edit_keyboard = InlineKeyboardMarkup(row_width=4)
+registration_edit_keyboard.row(
+    InlineKeyboardButton(text='Name', callback_data="registration_edit_keyboard=name"),
+    InlineKeyboardButton(text='Surname', callback_data="registration_edit_keyboard=surname"),
+).row(
+    InlineKeyboardButton(text='Phone', callback_data="registration_edit_keyboard=phone"),
+    InlineKeyboardButton(text='Email', callback_data="registration_edit_keyboard=email")
+).add(
+    InlineKeyboardButton(text='all again', callback_data="registration_edit_keyboard=all"),
+)
